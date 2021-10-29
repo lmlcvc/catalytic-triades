367de3b193c0749c7acdd724b41ed82bbc3eebac
lana <lanamilicevic10@gmail.com>
2021-10-29 16:55:44 +0200

    Initial commit

---
+++ clean_files.py
@@ -0,0 +1,36 @@
+import os
+import configparser
+
+# Ukloniti svaki atom koji nije:
+# * OG u SER
+# * SG u CYS
+# * CG u HIS/ASP/GLU
+# * OD1 ili OD2
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+config = config['default']
+
+dirpath = config['location']
+destpath = config['transformed_location']
+keypath = config['location'] + config['keywords']
+
+
+with open(keypath, 'r') as keys:
+    key_lines = [s.strip() for s in keys.readlines()]
+
+for filename in os.listdir(dirpath):
+    if filename.endswith(".pdb"):
+        file_fullname = os.path.join(dirpath, filename)
+        destname = os.path.join(destpath, filename)
+
+        with open(file_fullname, 'r') as file:
+            atoms = file.readlines()
+            with open(destname, 'w') as dest:
+                for key in key_lines:
+                    text = ""
+                    for atom in atoms:
+                        if key in atom[11:20].strip().replace(" ", ""):
+                            text += atom
+                    if len(text) > 0:
+                        dest.write(text)
---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+dok ima fileova, učitaj file s lkacije
+
+za svaki nuc:
+    za svaki base
+    ako je base unutar dozvoljene udaljenosti
+        za svaki acid
+
+    nađi iduć
+"""
