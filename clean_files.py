05074d5626b5ccf6a3e497042b85aa565eed6de5
Lana <59740928+lmlcvc@users.noreply.github.com>
2022-01-27 18:12:16 +0100

    Updated clean_files to match main

--- clean_files.py
+++ clean_files.py
@@ -7,30 +7,34 @@ import configparser
 # * CG u HIS/ASP/GLU
 # * OD1 ili OD2
 
-config = configparser.ConfigParser()
-config.read('config.ini')
-config = config['default']
+def clean_files():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    config = config['default']
 
-dirpath = config['location']
-destpath = config['transformed_location']
-keypath = config['location'] + config['keywords']
+    dirpath = config['location']
+    destpath = config['transformed_location']
+    keypath = config['location'] + config['keywords']
 
+    if not os.path.isdir(destpath):
+        os.makedirs(destpath)
 
-with open(keypath, 'r') as keys:
-    key_lines = [s.strip() for s in keys.readlines()]
+    with open(keypath, 'r') as keys:
+        key_lines = [s.strip() for s in keys.readlines()]
 
-for filename in os.listdir(dirpath):
-    if filename.endswith(".pdb"):
-        file_fullname = os.path.join(dirpath, filename)
-        destname = os.path.join(destpath, filename)
+    for filename in os.listdir(dirpath):
+        if filename.endswith(".pdb"):
+            file_fullname = os.path.join(dirpath, filename)
+            destname = os.path.join(destpath, filename)
 
-        with open(file_fullname, 'r') as file:
-            atoms = file.readlines()
-            with open(destname, 'w') as dest:
-                for key in key_lines:
-                    text = ""
-                    for atom in atoms:
-                        if key in atom[11:20].strip().replace(" ", ""):
-                            text += atom
-                    if len(text) > 0:
-                        dest.write(text)
+            with open(file_fullname, 'r') as file:
+                atoms = file.readlines()
+                with open(destname, 'w') as dest:
+                    for key in key_lines:
+                        text = ""
+                        for atom in atoms:
+                            if key in atom[11:20].strip().replace(" ", "") \
+                                    and 'ASN' not in atom[11:20].strip().replace(" ", ""):
+                                text += atom
+                        if len(text) > 0:
+                            dest.write(text)
